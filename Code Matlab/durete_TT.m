3189418f8a0b230f2994ce751b3284ad87f37d92
Dave-999 <david.dispas@gmx.fr>
2018-04-27 17:28:51 +0200

    Granulo.m

--- Code Matlab/durete_TT.m
+++ Code Matlab/durete_TT.m
@@ -10,11 +10,11 @@
  figure;
  c = categorical({'1','2','3','4','5'});
  hold on
-Durete = [139.9,150.95,133.65,118.65;86.7];
+Durete = [139.9,150.95,133.65,118.65,86.7];
  bar(Durete);
  %bar([1 2 3 4 5],Archp);
 %errorbar([0.85 1.15;1.85 2.15;2.85 3.15;3.85 4.15;4.85 5.15],Densite,[0.03 0.04; 0.01 0.04; 0.06 0.08; 0.07 0.03; 0.07 0.08],'o');
 errorbar([1 2 3 4 5],Durete,[0.0,3.20,3.82,0.84,0.86],'o');
-set(gca, 'XTick', 1:5,'XTickLabel',{'No HT','T150-2;'TT-200';'TT-250';'TT-300'});
+set(gca, 'XTick', 1:5,'XTickLabel',{'No HT','TT150-2','TT-200','TT-250','TT-300'});
 %ylim([99 100])
 ylabel('Vickers hardness [HV]');
