ab216916a250aecbd50696b619a31ce017d8fe6d
Arthur <bouillotarthur@hotmail.com>
2018-05-09 18:45:42 +0200

    methodes heat

--- Code Matlab/durete_TT.m
+++ Code Matlab/durete_TT.m
@@ -2,19 +2,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%
 %% BATCH X200-180220 -- Memoire %%
 %%%%%%%%%%%%%%%%%%%%%%%
-%Script permettant d'afficher divers graphes renseignant à propos de
-%la densité et la dureté des échantillons produits lors du batch X200-180220
+%Script permettant d'afficher divers graphes renseignant ï¿½ propos de
+%la densitï¿½ et la duretï¿½ des ï¿½chantillons produits lors du batch X200-180220
 
 
-%Comparaison des techniques de mesure de dureté
+%Comparaison des techniques de mesure de duretï¿½
  figure;
  c = categorical({'1','2','3','4','5'});
  hold on
-Durete = [139.9,150.95,133.65,118.65,86.7];
+Durete = [139.9,150.95,133.65,118.65,86.7,91.2,70.6];
  bar(Durete);
  %bar([1 2 3 4 5],Archp);
 %errorbar([0.85 1.15;1.85 2.15;2.85 3.15;3.85 4.15;4.85 5.15],Densite,[0.03 0.04; 0.01 0.04; 0.06 0.08; 0.07 0.03; 0.07 0.08],'o');
-errorbar([1 2 3 4 5],Durete,[0.0,3.20,3.82,0.84,0.86],'o');
-set(gca, 'XTick', 1:5,'XTickLabel',{'No HT','TT150-2','TT-200','TT-250','TT-300'});
+errorbar([1 2 3 4 5],Durete,[0.0,3.20,3.82,0.84,0.86,2.75,0.52],'o');
+set(gca, 'XTick', 1:5,'XTickLabel',{'No HT','TT150-2','TT200-2','TT250-2','TT300-2','TT300-1','TT360-1'});
 %ylim([99 100])
 ylabel('Vickers hardness [HV]');
